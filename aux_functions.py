9a06948abed62e936d5af716831e2180b65a42c1
Matheus Carvalho do Carmo <matheuscarvalho@windowslive.com>
2022-12-08 20:14:23 -0300

    first commit

---
+++ aux_functions.py
@@ -0,0 +1,66 @@
+import math
+from entities.vertex import Vertex
+
+def eucl_dist(x1,y1,x2,y2):
+    return math.sqrt( (x1-x2)**2 + (y1-y2)**2 )
+
+
+def calculate_route_distance(route, G):
+
+    routeDistanceCost = 0
+    for r in range(len(route)):
+        if r < len(route) -1:
+            i = route[r]['id']
+            j = route[r + 1]['id']
+        else:
+            i = route[r]['id']
+            j = route[0]['id']
+        routeDistanceCost += G.edges[i,j]['length']
+    return routeDistanceCost
+
+def calculate_penalties(route, G):
+
+    penalties = 0
+    for i in range(len(G.nodes)):
+        if G.nodes[i] not in route:            
+            penalties += G.nodes[i]['penalty']
+        
+    return penalties
+
+def calculate_bonus_colected(route, G):
+
+    bonus = 0
+    for r in route:        
+        bonus += G.nodes[r['id']]['bonus']
+
+    return bonus
+
+
+def swap_2_opt(i, j, route):
+    route_A = route[:i]
+    route_B = route[i:j]
+    route_B.reverse()
+    new_route = [*route_A,*route_B, *route[j:]]
+    return new_route
+
+def bonus_labels(route):
+    bonus_label = {}
+    for i in range(len(route)):
+        bonus_label[route[i]['id']] = route[i]['bonus']
+    return bonus_label
+
+
+def load_dataset(file_name):
+    dataset = []
+    is_coord = False
+    with open(file_name, "r") as f:
+        for line in f:
+            new_line = line.strip()  # remove spaces at the beginning and the end if they are available
+            if(new_line == 'NODE_COORD_SECTION'):
+                is_coord = True
+                continue
+            if(is_coord):
+                new_line = new_line.split(" ")  # split a string into a list
+                id, x, y = new_line[0], new_line[1], new_line[2]  # check dataset file to see why id,x,y = 0,1,2
+                dataset.append(Vertex(id=id, x=x, y=y))  # Create a Node object with id, x, y and add to the data list
+    return dataset
---
+++ entities/vertex.py
@@ -0,0 +1,5 @@
+class Vertex:
+    def __init__(self, id, x, y):
+        self.id = int(id)
+        self.x = float(x)
+        self.y = float(y)
